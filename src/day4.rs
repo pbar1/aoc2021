21bfdb16766e657ba90cb48b2ae9b2023ed70ef4
Pierce Bartine <piercebartine@gmail.com>
2021-12-05 16:47:51 -0600

    wip day 4

---
+++ src/day4.rs
@@ -0,0 +1,19 @@
+use std::ops::{Deref, Index};
+
+use crate::util;
+
+pub fn part1() {
+    let filename = "./input/day4.txt";
+
+    let result = "unimpl";
+
+    println!("day4part1: {}", result);
+}
+
+pub fn part2() {
+    let filename = "./input/day4.txt";
+
+    let result = "unimpl";
+
+    println!("day4part2: {}", result);
+}
