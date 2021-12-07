7efc15b57aee5b806cccc43d468de3736f5270a5
Pierce Bartine <piercebartine@gmail.com>
2021-12-06 21:25:42 -0600

    Add day 6 part 2 with optimization

--- src/day6.rs
+++ src/day6.rs
@@ -1,33 +1,9 @@
-use std::{cell::Cell, fs};
+use std::fs;
 
 pub fn part1() {
     let filename = "./input/day6.txt";
-    let mut fish: Vec<Cell<u8>> = fs::read_to_string(filename)
-        .unwrap()
-        .trim()
-        .split(',')
-        .map(|s| {
-            let n: u8 = s.parse().unwrap();
-            Cell::new(n)
-        })
-        .collect();
 
-    for _ in 0..80 {
-        let mut spawned = Vec::new();
-        for f in &fish {
-            let mut countdown = f.get();
-            if countdown == 0 {
-                countdown = 6;
-                spawned.push(Cell::new(8u8));
-            } else {
-                countdown = countdown - 1;
-            }
-            f.set(countdown);
-        }
-        fish.append(&mut spawned);
-    }
-
-    let result = fish.len();
+    let result = simulate(filename, 80);
 
     println!("day6part1: {}", result);
 }
@@ -49,7 +25,7 @@ fn simulate(filename: &str, days: i32) -> u64 {
         .map(|s| s.parse::<usize>().unwrap())
         .for_each(|age| age_counts[age] += 1);
 
-    for _ in 0..256 {
+    for _ in 0..days {
         age_counts.rotate_left(1);
         age_counts[6] += age_counts[8];
     }
